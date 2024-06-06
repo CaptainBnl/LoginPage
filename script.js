8ad568361fee6c69b48f91274414f424202de4e8
Captain Bnl <sagarmondal3773@gmail.com>
2024-06-06 11:28:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-const closeIcon = document.querySelector("#close-icon");
-const menuIcon = document.querySelector("#menu-icon");
+const menu = document.querySelector("#menu-icon");
+const close_icon = document.querySelector("#close-icon");
 const tl = gsap.timeline();
 
-tl.to(".overlay", {
-  right: 0,
-  duration: 0.7,
-})
+  tl.to(".overley", {
+    right: 0,
+    duration: 0.7,
+  })
 
-tl.from(".overlay .links h4", {
-  x: 200,
-  duration: 0.8,
-  stagger: .2,
-  opacity: 0,
-})
-tl.from(closeIcon, {
-  opacity: 0,
-})
+  tl.from(".overley .links h3", {
+    x: 150,
+    duration: 0.7,
+    stagger: 0.2,
+    opacity: 0,
+  })
 
-tl.pause();
+  tl.from(".overley .icons i", {
+    opacity: 0,
+  })
 
-menuIcon.addEventListener("click", () => tl.play())
+  tl.pause()
 
-closeIcon.addEventListener("click", () => tl.reverse())
+  menu.addEventListener("click", () => {
+    tl.play()
+  })
+
+  close_icon.addEventListener("click", () => {
+    tl.reverse()
+  })
